90d2d3a22fb353428b713d8a8e4bab9432aa808d
logpacket <199sd@naver.com>
2017-11-25 22:00:45 +0900

    이번엔 제발

--- Project1/Project1/DBfunc.c
+++ Project1/Project1/DBfunc.c
@@ -2,20 +2,6 @@
 #include<string.h>
 #include<stdlib.h>
 #define BUF_SIZE 1024
-int save(char *filepath, char *value) {
-	FILE *fp = NULL;
-	if ((fp = fopen(filepath, "a")) == NULL) {
-		return -1;
-	}
-	if (fputs(value, fp) == EOF) {
-		fclose(fp);
-		return -1;
-	}
-	else {
-		fclose(fp);
-		return 0;
-	}
-}
 int rm(char *filepath, char *key) {
 	int i = 0;
 	char temp[BUF_SIZE];
@@ -46,11 +32,9 @@ int rm(char *filepath, char *key) {
 		remove("temp.txt");
 		return -1;
 	}else if (remove(filepath)<0) {
-		printf("실패");
 		return -1;
 	}
 	rename("temp.txt", filepath);
-	printf("완료");
 	return 0;
 }
 int edit(char *filepath, char  *key, char * value) {
@@ -91,37 +75,58 @@ int edit(char *filepath, char  *key, char * value) {
 	return 0;
 }
 char * read(char *filepath, char *key) {
+	int i = 0;
+	char temp[BUF_SIZE];
 	char *temp2 = (char*)malloc(sizeof(char)*BUF_SIZE);
-	char *temp = (char*)malloc(sizeof(char)*BUF_SIZE);
 	char * tempkey;
 	FILE *fp = NULL;
 	if ((fp = fopen(filepath, "r")) == NULL) {
 		return NULL;
 	}
 	while (fgets(temp,BUF_SIZE,fp)) {
-		if (temp == NULL) {
-			return NULL;
-		}
 		strcpy(temp2, temp);
 		tempkey = strtok(temp,":");
-		if (tempkey == key) {
+		if (!strcmp(tempkey,key)) {
+			i = 1;
 			break;
 		}
 	}
 	fclose(fp);
-	return temp2;
+	char * result = (i == 0)?NULL : temp2;
+	return result;
 }
+
+int save(char *filepath, char *value) {
+	char * key = (char*)malloc(sizeof(char)*BUF_SIZE);
+	FILE *fp = NULL;
+	if ((fp = fopen(filepath, "a")) == NULL) {
+		return -1;
+	}
+	key = strtok(value, ":");
+	if (read(filepath, key) != NULL) return -1;
+	if (fputs(value, fp) == EOF) {
+		fclose(fp);
+		return -1;
+	}
+	else {
+		fclose(fp);
+		return 0;
+	}
+}
+/*
+void main() {
 	//save함수 사용시에 뒤에 개행문자 꼭넣을것
 	//save("test.txt", "ls:ls:ls\n");
-	
+
 	//이건 그냥 쓰면됨
 	//rm("test.txt", "ls");
-	
+
 	//이것도 value dp 개행문자 꼭넣을것
 	//edit("test.txt", "ls", "af:af:af\n");
 
 	//read함수 사용의 올바른 예
 	//오류시 NULL값 반환
-	/*char *n = read("test.txt", "af");
+	char *n = read("test.txt", "ls");
 	printf("\n%s",n);
-	free(n);*/
+	free(n);
+}*/
